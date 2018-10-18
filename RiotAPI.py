fa35735916f5197470f4809d5d509a7a333686ff
Rudengs <rudengkim@gmail.com>
2018-10-18 22:54:30 +0900

    Riot API 돌려보는중

--- RiotAPI.py
+++ RiotAPI.py
@@ -1,29 +1,63 @@
 import requests
+import pprint
+import json
 
-def requestSummonerData(region, summonerName, APIKey):
-    URL = "https://" + region + ".api.riotgames.com/lol/summoner/v3/summoners/by-name/" + summonerName + "?api_key=" + APIKey
-    response = requests.get(URL)
-    return response.json()
+def getSummonerData(region, summonerName, APIKey):
+        """소환사의 프로필 정보"""
+        URL = "https://" + region + ".api.riotgames.com/lol/summoner/v3/summoners/by-name/" + summonerName + "?api_key=" + APIKey
+        response = requests.get(URL)
+        return response.json()
 
-def requestRankedData(region, summonerName, APIKey):
-    URL = "https://" + region + ".api.riotgames.com/lol/league/v3/positions/by-summoner/" + summonerName + "?api_key=" + APIKey
-    response = requests.get(URL)
-    return response.json()
+def getRankedData(region, summonerID, APIKey):
+        """자유랭, 솔랭 데이터"""
+        URL = "https://" + region + ".api.riotgames.com/lol/league/v3/positions/by-summoner/" + summonerID + "?api_key=" + APIKey
+        response = requests.get(URL)
+        return response.json()
+
+def getChampionMasteryDatas(region, summonerID, APIKey):
+        """챔피언의 숙련도 데이터"""
+        URL = "https://" + region + ".api.riotgames.com/lol/champion-mastery/v3/champion-masteries/by-summoner/" + summonerID + "?api_key=" + APIKey
+        response = requests.get(URL)
+        return response.json()
+
+def getChampionMasteryData(region, summonerID, championId, APIKey):
+        """특정 챔피언의 숙련도 데이터"""
+        URL = "https://" + region + ".api.riotgames.com/lol/champion-mastery/v3/champion-masteries/by-summoner/" + summonerID + "/by-champion/" + championId + "?api_key=" + APIKey
+        response = requests.get(URL)
+        return response.json()
+
+def getChampionScoreData(region, summonerID, APIKey):
+        """챔피언의 숙련도 총합"""
+        URL = "https://" + region + ".api.riotgames.com/lol/champion-mastery/v3/score/by-summoner/" + summonerID + "?api_key=" + APIKey
+        response = requests.get(URL)
+        return response.json()       
 
 def main():
-    region = (str)(input('Region: '))
-    summonerName = (str)(input('Summoner Name: '))
+        region = (str)(input('Region: '))
+        summonerName = (str)(input('Summoner Name: '))
 
-    f = open("./DEVELOPMENT_API_KEY.txt", 'r')
-    APIKey = f.readline()
-    f.close()
+        with open("./DEVELOPMENT_API_KEY.txt", 'r') as fout:
+                APIKey = fout.readline()
 
-    summonerDataJSON = requestSummonerData(region, summonerName, APIKey)
-    ID = str(summonerDataJSON['id'])
-    RankedDataJSON = requestRankedData(region, ID, APIKey)
+        summonerDataJSON = getSummonerData(region,summonerName,APIKey)
+        summonerID = str(summonerDataJSON['id'])
+        jsonData = getRankedData(region, summonerID, APIKey)
+        print(json.dumps(jsonData, indent=2))
 
-    print (RankedDataJSON[1]['tier'])
-    print (RankedDataJSON[1]['rank'])
+def printRankData():
+        region = (str)(input('Region: '))
+        summonerName = (str)(input('Summoner Name: '))
+
+        f = open("./DEVELOPMENT_API_KEY.txt", 'r')
+        APIKey = f.readline()
+        f.close()
+
+        summonerDataJSON = getSummonerData(region, summonerName, APIKey)
+        ID = str(summonerDataJSON['id'])
+        RankedDataJSON = getRankedData(region, ID, APIKey)
+
+        print (RankedDataJSON[1]['tier'])
+        print (RankedDataJSON[1]['rank'])
 
 if __name__ == "__main__":
     main()
